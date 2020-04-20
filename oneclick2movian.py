6de1928de072a36056c3045ed47f79ff75081668
Nikolaos Pap <nikkpap@gmail.com>
2020-04-20 20:02:47 +0300

    Update oneclick2movian.py

--- oneclick2movian.py
+++ oneclick2movian.py
@@ -1,8 +1,6 @@
-####################################################################
-# Import modules
-####################################test################################
+#!/usr/bin/env python
+
 import requests
-import http.server
 import socketserver
 import os
 import socket
@@ -10,147 +8,258 @@ import webbrowser
 import threading
 import time
 import platform
+import threading
+import queue as Queue
+import subprocess as commands # only python3
+import subprocess
+import sys
+from http.server import HTTPServer
+from http.server import SimpleHTTPRequestHandler
 from tkinter import *
 from tkinter import ttk
-from tkinter.filedialog import askopenfilename
+from tkinter.filedialog import *
 from tkinter import messagebox
 from tkinter.ttk import *
 from pythonping import ping
 
 
-# Initialize first window0
-window0 = Tk()
-
-####################################################################
-# Variable Declarations
-####################################################################
-window0.title("Movian One Click Plugin Installer v0.1")
-window0.geometry('450x150')
-window0.resizable(False, False)
-lbl = Label(window0, text="Movian IP")
-lbl.place(x=200, y=23)
-lb2 = Label(window0, text="by nikkpap")
-lb2.place(x=200, y=46)
-txt_box1 = Entry(window0, width=30)
-txt_box1.insert(END, '192.168.8.100')
-txt_box1.place(x=3, y=23)
-txt_box2 = Entry(window0, width=30)
-txt_box2.insert(END, 'http://')
-txt_box2.place(x=3, y=46)
-is_windows = any(platform.win32_ver())
-
-#########################END OF GUI ################################
-#print("Your Computer IP Address is: " + IPAddr)
-ip_movian = txt_box1.get()
-port_movian = '42000'
-url_movian = (f'http://{ip_movian}:{port_movian}')
-period_of_time = 3  # sec
-hostname = socket.gethostname()
-IPAddr = socket.gethostbyname(hostname)
-def_path = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Create a TCP/IP socket
-
-####################################################################
-# Functions
-####################################################################
-
-# File manager function
 
 
-def OpenFile():
-
-    name = askopenfilename(initialdir="" + def_path, filetypes=(
-        ("Zip File", "*.zip"), ("All Files", "*.*")), title="Choose a Plugin")
-    dir_to_server = os.path.dirname(name)
-    base_name = os.path.basename(name)
-    startHTTPServer(dir_to_server)
-    webbrowser.open(f'{url_movian}/?url=http://{IPAddr}:8080/{base_name}')
-    # Using try in case user types in unknown file or closes without choosing a file.
-    try:
-        with open(name, 'r') as UseFile:
-            print(UseFile.read())
-    except:
-        messagebox.showinfo('Error', 'Cancel')
+class ServerThread(threading.Thread):
 
 
-def startHTTPServer(dir_to_server):
-    PORT = 8080
-    os.chdir(dir_to_server)
-    Handler = http.server.SimpleHTTPRequestHandler
-    httpd = socketserver.TCPServer(("", PORT), Handler)
-    print("serving at port", PORT)
-    # webbrowser.open(f'{url_movian}/?url=http://{IPAddr}:8080/torrent.zip') ????????????
-    # can i use a timer to stop the server after 5 sec
-    httpd.serve_forever()
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.queue = server_queue
+        self.is_server_running = False 
+        self.daemon = False
 
 
-def test_ip():
-    rep = os.system('ping ' + ip_movian)
-    if rep == 0:
-        print('server is up')
-        lbl.configure(text="Connection Established... !!")
+    def create_server(self, port=8000):
+        self.server = HTTPServer((ip_address, port), SimpleHTTPRequestHandler)
+        self.serv_info = self.server.socket.getsockname()
+
+
+    def run(self):
+        while True:
+            if not self.queue.empty():
+                i = self.queue.get()
+                print(i)
+                if i[0] == 'start':
+                    self.create_server(port=int(i[1]))
+                    self.is_server_running = True
+                    print('SERVING @: {}:{}'.format(self.serv_info[0],self.serv_info[1]))
+                    self.server.serve_forever()
+                elif i[0] == 'stop':
+                    self.is_server_running=False
+                    print('SERVER SHUTDOWN')
+                    self.server = None
+
+
+
+
+
+
+class Window(Frame):
+    def __init__(self, def_path, master=None):
+        Frame.__init__(self, master)
+        self.master = master
+
+        self.def_path = def_path
+
+        # Menu Bar GUI
+        menu = Menu(self.master)
+        self.master.config(menu=menu)
+
+        fileMenu = Menu(menu)
+        fileMenu.add_command(label='Mode.Zip', command=self.mode_zip)
+        fileMenu.add_command(label='Log.File', command=self.save_log)
+        fileMenu.add_command(label='Test.IP', command=self.test_ip)
+        fileMenu.add_command(label="Exit", command=self.exit_program)
+        menu.add_cascade(label="File", menu=fileMenu)
+
+        editMenu = Menu(menu)
+        editMenu.add_command(label="Undo")
+        editMenu.add_command(label="Redo")
+        menu.add_cascade(label="Edit", menu=editMenu)
+
+        # Commands - Buttons GUI
+        self.pack(fill=BOTH, expand=1)
+        btn0 = Button(self, text="Test IP", command=self.test_ip)
+        btn0.place(x=10, y=110)
+        btn1 = Button(self, text="Save Log", command= self.save_log)
+        btn1.place(x=93, y=110)
+        btn2 = Button(self, text="Choose Zip", command= self.mode_zip)
+        btn2.place(x=196, y=110)
+        btn3 = Button(self, text="Exit", command=self.exit_program)
+        btn3.place(x=288, y=110)
+        btn4 = Button(self, text="About", command=self.about)
+        btn4.place(x=371, y=110)
+        rad1 = Radiobutton(self, text='.Zip   ', value=1, state='enable')
+        rad1.grid(column=1, row=0)
+        rad2 = Radiobutton(self, text='Http://', value=2, state='enable')
+        rad2.grid(column=2, row=0)
+
+        self.lbl = Label(self, text="...")
+        self.lbl.place(x=300, y=23)
+        self.lb2 = Label(self, text="by nikkpap")
+        self.lb2.place(x=300, y=46)
+        self.txt_box1 = Entry(self, width=30)
+        self.txt_box1.insert(END, '10.0.0.6')
+        self.txt_box1.place(x=3, y=23)
+        txt_box2 = Entry(self, width=30)
+        txt_box2.insert(END, 'http://')
+        txt_box2.place(x=3, y=46)
+        self.port_movian = '42000'
+
+
+   
+        
+
+    # def mode_url():
+    #     print ("mode_url")
+    #     btn2.configure(text = "Send Link", command=mode_zip)
+
+
+
+    def mode_zip(self):
+
+        ip_movian = self.txt_box1.get()
+        name = askopenfilename(initialdir="" + self.def_path, filetypes=(("Zip File", "*.zip"), ("All Files", "*.*")), title="Choose Zip Plugin")
+        dir_to_server = os.path.dirname(name)
+        base_name = os.path.basename(name)
+        os.chdir(dir_to_server)
+
+        port=0
+        server_queue.put(('start', port))
+        # wait for the server_thread to switch is_server_running = True
+        while not server_thread.is_server_running:
+            pass
+        p = server_thread.serv_info[1]
+
+        webbrowser.open(f'http://{ip_movian}:{self.port_movian}/showtime/open?url=http://{ip_address}:{p}/{base_name}', new=1, autoraise=True)
+        self.lbl.configure(text=f'http://{ip_address}:{p}/{base_name}')
+
+
+    def test_ip(self):
+
+        ip_movian = self.txt_box1.get()
+        if os.name == 'nt':
+            #rep = os.system("ping -n 1 %s" % ip_movian)
+            rep = str(subprocess.Popen(["ping.exe",ip_movian],stdout = subprocess.PIPE).communicate()[0], 'utf-8')
+            print(rep)
+            
+            if ('unreachable' in rep):
+                print('server is down')
+                self.lbl.configure(text="No Connection... !!", background= "red")
+            else: 
+                print('server is up')
+                self.lbl.configure(text="Connection OK... !!", background= "green")
+        else :
+            rep = os.system('ping -w 1 %s' % ip_movian)
+
+            if rep == 0:
+                print('server is up')
+                self.lbl.configure(text="Connection OK... !!", background= "green")
+            else:
+                print('server is down')
+                self.lbl.configure(text="No Connection... !!", background= "red")
+
+
+    def save_log(self):
+
+        ip_movian = self.txt_box1.get()
+
+        print (ip_movian)
+
+        try:
+            response = requests.get(f'http://{ip_movian}:{self.port_movian}', timeout=10 )
+        except:
+            self.lbl.configure(text="No Connection... !!")
+            return
+
+        url_movian = (f'http://{ip_movian}:{self.port_movian}')
+        recieve = requests.get(f'{url_movian}/api/logfile/0')
+        f = asksaveasfile(mode='wb' , filetypes=(("Log File", "*.log"), ("All Files", "*.*")), title="Save Log File" )
+        if f is None: # asksaveasfile return `None` if dialog closed with "cancel".
+            print("askforfile failed")
+            return
+        f.write(recieve.content)
+        f.close()
+        print('Log file saved OK... !')
+        self.lbl.configure(text="Log file saved OK... !!")
+
+    def mode_url(self):
+
+        ext_url_base_name = self.txt_box2.get()
+                
+        if len(ext_url_movian) >= 7 :
+            bnt2.configure(text = "Send Link", command=mode_url)
+            webbrowser.open(f'http://{ip_movian}:{self.port_movian}/showtime/open?url={ext_url_base_name}', new=1, autoraise=True)
+            self.lbl.configure(text=f'{ext_url_base_name}')
+        else:
+
+            self.lbl.configure(text="Put the correct URL or use Zip mode... !!")
+
+   
+
+    def about(self):
+        messagebox.showinfo('About', 'Movian One Click Plugin Installer by nikkpap\n with the great help of Czz78 (thanks man)\n <<for MovianGR and Movian on Telegram>>\n ALU DEV TEAM @ 2019-20 ')
+
+
+    def exit_program(self):
+        server_queue.put(('stop', ''))
+        os._exit(os.EX_OK)
+
+
+
+
+
+# get my ip address
+def get_ip_address():
+    if os.name == 'posix':
+        ip = commands.getoutput("hostname -I")
+    elif os.name == 'nt':
+        ip = socket.gethostbyname(socket.gethostname())
     else:
-        print('server is down')
-        lbl.configure(text="No Connection... !!")
+        ip = ''
+        print('Couldn\'t get local ip')
+    return ip
 
 
-def down_log():
-    response = requests.get(f'http://{ip_movian}:{port_movian}')
-    if response:
-        print('Download OK... !')
-        recieve = requests.get(f'{url_movian}/api/logfile/0?mode=download')
-        with open(f'{def_path}\movian0.log', 'wb') as fo:
-            fo.write(recieve.content)
-        lbl.configure(text="Download OK... !!")
+
+if __name__ == "__main__":
+
+    home = os.curdir
+    if 'HOME' in os.environ:
+        home = os.environ['HOME']
+    elif os.name == 'posix':
+        home = os.path.expanduser("~/")
+    elif os.name == 'nt':
+        if 'HOMEPATH' in os.environ and 'HOMEDRIVE' in os.environ:
+            home = os.environ['HOMEDRIVE'] + os.environ['HOMEPATH']
     else:
-        # print('An error has occurred.')
-        lbl.configure(text="No Connection... !!")
+        home = os.environ['HOMEPATH']
 
 
-def install_plugin():
-    lbl.configure(text="Not yet... !!")
+    if os.name == 'nt':
+        ip_address = get_ip_address()
+    else:
+        ip_address = get_ip_address().split()[0] # this function on windows returns other ip, this is in array because in linux you can have more ip in sequence, i don't remember on win
+
+    print(ip_address)
 
 
-def about():
-    messagebox.showinfo(
-        'About', 'Movian One Click Plugin Installer by nikkpap @ 2019')
-
-####################################################################
-    # Menu Bar GUI
-####################################################################
-
-
-menu = Menu(window0)
-window0.config(menu=menu)
-
-file = Menu(menu)
-file.add_command(label='Open', command=OpenFile)
-file.add_command(label='D.Log', command=down_log)
-file.add_command(label='Test.IP', command=test_ip)
-file.add_command(label='Exit', command=lambda: exit())
-menu.add_cascade(label='File', menu=file)
-
-####################################################################
-# Commands - Buttons GUI
-####################################################################
-
-btn0 = Button(window0, text="Test IP", command=test_ip)
-btn0.place(x=10, y=110)
-btn1 = Button(window0, text="Download Log", command=down_log)
-btn1.place(x=90, y=110)
-btn2 = Button(window0, text="Choose Path", command=OpenFile)
-btn2.place(x=180, y=110)
-btn3 = Button(window0, text="Exit", command=exit)
-btn3.place(x=260, y=110)
-btn4 = Button(window0, text="About", command=about)
-btn4.place(x=340, y=110)
-rad1 = Radiobutton(window0, text='.Zip   ', value=1, state='enable')
-rad1.grid(column=1, row=0)
-rad2 = Radiobutton(window0, text='Http://', value=2, state='enable')
-rad2.grid(column=2, row=0)
+    # Gui
+    root = Tk()
+    app = Window(home, root)
+    root.title("Movian One Click Plugin Installer v0.5")
+    root.geometry('500x150')
+    root.resizable(False, False)
 
 
-window0.mainloop()
-####################################################################
-# EOF
-####################################################################
+    server_queue = Queue.Queue()
+    server_thread = ServerThread()
+    server_thread.start()
+
+    root.mainloop()
